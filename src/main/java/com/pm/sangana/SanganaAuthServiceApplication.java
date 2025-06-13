2fce90ae583618ddb5fba43093206b41c41019d4
Tongesai C Machinya <machinyat@gmail.com>
2025-06-13 18:12:31 -0400

    first commit

---
+++ src/main/java/com/pm/sangana/SanganaAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pm.sangana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanganaAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SanganaAuthServiceApplication.class, args);
+    }
+
+}
